9b9f9d9121e2c015ed6cc4cd6eb3d1f9086da70e
lnelson <lnelson@parc.com>
2015-02-06 17:47:13 -0800

    upper case server tutorial

--- http_upper_server.js
+++ http_upper_server.js
@@ -1,17 +1,21 @@
 /**
  * Created by lnelson on 2/6/15.
  */
-/** @module http_file_server */
+/** @module http_upper_server */
 
 var http = require('http');
 var process = require('process');
 var fs = require('fs');
 var testing = true;
+var map = require('through2-map');
 
-function listen(port, path) {
+function listen(port) {
     var server = http.createServer(function (request, response) {
-        var file_stream = fs.createReadStream(path);
-        file_stream.pipe(response);
+        if (request.method == 'POST') {
+            request.pipe(map(function (chunk) {
+                return chunk.toString().toUpperCase();
+            })).pipe(response)
+        }
     });
     server.listen(port);
     //return server;
@@ -28,10 +32,13 @@ function get_argument_or_default(args, argument_index, a_default) {
 
 function action(args) {
     var on_the_port = value(get_argument_or_default(args, 2, 8910));
-    var file_path = get_argument_or_default(args, 3, "/Users/lnelson/WebstormProjects/learnyounode/seven_lines.txt");
-    listen(on_the_port, file_path);
-    //setTimeout(function () {process.exit();}, 5000);
+    listen(on_the_port);
+    setTimeout(function () {process.exit();}, 5000);
 }
+
+process.on('uncaughtException', function (err) {
+    console.log('Caught exception: ' + err);
+});
 
 
 action(process.argv);
